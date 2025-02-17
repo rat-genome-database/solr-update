58620ad47449e59eb929a5814d861cf6a0e6b351
jdepons <jdepons@mcw.edu>
2025-02-17 16:41:51 -0600

    updated

---
+++ src/main/dist/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. /etc/profile
+
+APPNAME=solr-updater
+APPDIR=/home/rgddata/pipelines/$APPNAME
+
+cd $APPDIR
+
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
+    -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
+    -jar lib/$APPNAME.jar "$@" 2>&1
